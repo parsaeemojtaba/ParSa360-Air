96e72507cdaa78460f4560d995033fa4b82808a1
Mojtaba Parsaee <48765348+parsaeemojtaba@users.noreply.github.com>
2023-05-25 18:17:54 -0400

    Add files via upload

---
+++ libs/ParSa360Air_LoggerRun.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+import sys
+LIB_PATH = '/home/pi/ParSa360-Air/libs'
+sys.path.insert(1, LIB_PATH)
+from ParSa360Air_Logger import ParSa360Air_Logger
+
+def main():
+    # datalogger_main_dir = '/home/pi/DataLogger/'
+    data_logger = ParSa360Air_Logger()
+    set_raspberry_pi = data_logger.get_input('Set Raspberry Pi:')
+    measurement_duration = data_logger.get_input_minutes('Set the duration of measurement in minutes:')
+    capture_duration = data_logger.get_input('Set the delay between each run in seconds:')
+    sensor_sleep_time = data_logger.get_input('Set the sensor sleep time between each capture in seconds:')
+    hqcamera_sleep_time = data_logger.get_input_minutes('Set the HQ camera sleep time between each capture in minutes:')
+    data_logger.main(set_raspberry_pi, measurement_duration, capture_duration, sensor_sleep_time, hqcamera_sleep_time)
+
+if __name__ == '__main__':
+    main()

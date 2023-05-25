126ce4912a989e9fb06b145b03efbd9e08674122
Mojtaba Parsaee <48765348+parsaeemojtaba@users.noreply.github.com>
2023-05-25 18:18:41 -0400

    Install.sh

--- Install.sh
+++ Install.sh
@@ -25,7 +25,7 @@ sudo apt-get upgrade
 
 ## move the run file to main dirctory
 echo "$(tput setaf 12)move the run file to main dirctory"
-sudo mv /home/pi/ParSa360-Air/libs/ParSaDataLoggerSet.py /home/pi/
+sudo mv /home/pi/ParSa360-Air/libs/ParSa360Air_LoggerRun.py /home/pi/
 
 ## set I2C ports
 chmod 777 /home/pi/ParSa360-Air/libs/setI2Cs.sh  
